314b1ad14501d55378586c2392bcbc7272796de4
Dmitry Krasnov <dmitrykrasnov@dkrasnov.dev>
2024-02-27 10:36:53 +1300

    BATMAN

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import "./App.css";
 import VideoJS from "./VideoPlayer";
 
 function App() {
   const videoJsOptions = {
     autoplay: true,
     controls: true,
-    responsive: true,
+    playsinline: true,
     preferFullWindow: true,
+    nativeControlsForTouch: false,
     sources: [
       {
         src: "https://storage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4",
@@ -15,7 +15,11 @@ function App() {
     ],
   };
 
-  return <VideoJS options={videoJsOptions} />;
+  return (
+    <main className="min-w-screen min-h-screen bg-black">
+      <VideoJS options={videoJsOptions} />
+    </main>
+  );
 }
 
 export default App;
